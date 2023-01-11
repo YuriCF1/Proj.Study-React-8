ad4fb50426d73ef3aca52e12231e47ff9106ce80
Yuri <yurifdev@gmail.com>
2023-01-11 19:58:17 -0300

    Creating the pages

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,15 +1,26 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 //INSTALANDO DEPENDÊNCIAS
 //npm i json-server react-router-dom
 
-import React from 'react' // Avoiding error: Error : 'React' must be in scope when using JSX react/react-in-jsx-scope
+import "./App.css";
+// 1 - Config react router
+import React from "react"; // Avoiding error: Error : 'React' must be in scope when using JSX react/react-in-jsx-scope
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Pages
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
     <div className="App">
+      {/* O que está fora de Routes, existirá em todas as páginas */}
       <h1>React Router</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
