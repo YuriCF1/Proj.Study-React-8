365c884cf0f74c926856a4f645bb81cdb523a056
Yuri <yurifdev@gmail.com>
2023-02-18 20:19:23 -0300

    Comenting differences between a tag, link and Navlink

--- react-router/src/components/Navbar.js
+++ react-router/src/components/Navbar.js
@@ -1,25 +1,30 @@
 // 2 - Links com react router
-import React from 'react'
+import React from "react";
 
-import "./Navbar.css"
+import "./Navbar.css";
 
-import { Link, NavLink} from "react-router-dom" 
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav>
-        {/* Redirecionamento trocando o componente*/}
-        {/* <Nav to="/">Home</Nav>
+      {/* Redirecionamento trocando o componente*/}
+      {/* <Nav to="/">Home</Nav>
         <Nav to="/about">Sobre</Nav> */}
 
-        {/* NavLink é a mesma coisa de Link, porém ele permite a atrobito IsActive */}
-        {/* Uma maneira de não utilizar a classe padrão 'active' */}
-        <NavLink to="/" /* className={({isActive}) => (isActive ? 'estaAtivo' : 'naoAtivo')}*/>Home</NavLink>
-        <NavLink to="/about">Sobre</NavLink>
-        {/* Redirecionamento fazendo reload  da página */}
-        {/* <a href=""></a> */} 
-    </nav> 
-  )
-}
+      {/* NavLink é a mesma coisa de Link, porém ele permite a atrobito IsActive */}
+      {/* <Link to="/about">Sobre</Link> */}
+      {/* Uma maneira de não utilizar a classe padrão 'active' */}
+      <NavLink
+        to="/" /* className={({isActive}) => (isActive ? 'estaAtivo' : 'naoAtivo')}*/
+      >
+        Home
+      </NavLink>
+      <NavLink to="/about">Sobre</NavLink>
+      {/* Redirecionamento fazendo reload  da página */}
+      {/* <a href=""></a> */}
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
